cbd7e8b59d6f7346872d5e10d4b2eb84da40be6b
Cristian Aguirre <cdaguirre@uc.cl>
2025-05-10 14:32:58 +0200

    refactor: rename login parser error

--- src/bank_client/login/parser.ts
+++ src/bank_client/login/parser.ts
@@ -1,3 +1,4 @@
+import { BankClientParsingError } from '../error';
 import type { LoginResponse } from './types';
 
 export function parseLoginResponse(response: LoginResponse | unknown): string {
@@ -7,5 +8,5 @@ export function parseLoginResponse(response: LoginResponse | unknown): string {
       return loginResponse.token.access_token;
     }
   }
-  throw new Error(`[Parsing error]: Invalid login response. Response: ${JSON.stringify(response)}`);
+  throw new BankClientParsingError(`[Login parser]: Invalid login response. Response: ${JSON.stringify(response)}`);
 }
