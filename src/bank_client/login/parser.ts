5d92bc0dc1f4d9b7fda83b522888048992c87322
Cristian Aguirre <cdaguirre@uc.cl>
2025-05-10 13:14:57 +0200

    feat: add login parser

---
+++ src/bank_client/login/parser.ts
@@ -0,0 +1,11 @@
+import type { LoginResponse } from './types';
+
+export function parseLoginResponse(response: LoginResponse | unknown): string {
+  if (typeof response === 'object' && response !== null && 'token' in response) {
+    const loginResponse = response as LoginResponse;
+    if (loginResponse.token && typeof loginResponse.token.access_token === 'string') {
+      return loginResponse.token.access_token;
+    }
+  }
+  throw new Error(`[Parsing error]: Invalid login response. Response: ${JSON.stringify(response)}`);
+}
